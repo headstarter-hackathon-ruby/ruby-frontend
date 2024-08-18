f938fc72965302c14f5ba431d14cb6e3c2555766
harvey <harveytseng2@gmail.com>
2024-08-17 21:24:35 -0400

    creates basic ui for displaying resolved and unresolved complaints by user

--- src/app/requests/page.tsx
+++ src/app/requests/page.tsx
@@ -1,11 +1,130 @@
-import react from "react";
+import React from "react";
 import { Pinecone } from "@pinecone-database/pinecone";
 import RequestsUI from "./ui";
 import { createClient } from "../utils/supabase/server";
+import { API_URL } from "../config";
 
-const fetchComplaints = async (userId: string) => {};
+const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! });
+const supabase = createClient();
 
-export default function RequestsPage() {
-  const complaints = fetchComplaints();
-  return <RequestsUI complaints={complaints} />;
+const fetchComplaints = async (userId: string) => {
+  const index = pc.index("complaints");
+
+  const queryResponse = await index.namespace("rag_complaints").query({
+    vector: Array(1536).fill(0),
+    topK: 10000,
+    includeMetadata: true,
+    filter: {
+      userID: { $eq: userId },
+    },
+  });
+
+  const complaints = queryResponse.matches
+    .map((match) => {
+      if (match.metadata) {
+        return {
+          pinecodeID: match.id,
+          admin_text: match.metadata.admin_text.toString(),
+          product: match.metadata.product.toString(),
+          resolved: match.metadata.resolved.toString(),
+          subcategory: match.metadata.subcategory.toString(),
+          summary: match.metadata.summary.toString(),
+          text: match.metadata.text.toString(),
+          userID: match.metadata.userID.toString(),
+        };
+      }
+      return null;
+    })
+    .filter((complaint) => complaint !== null);
+
+  return complaints;
+};
+
+const fetchSimilarComplaints = async (complaint: string) => {
+  "use server";
+  const response = await fetch(
+    `${API_URL}complaints/similar?complaint=${encodeURIComponent(complaint)}`
+  );
+
+  if (response.ok) {
+    const similarComplaints = await response.json();
+    return similarComplaints;
+  } else {
+    console.error("Error fetching similar complaints:", response.statusText);
+    return [];
+  }
+};
+
+const fetchSolution = async (complaint: string) => {
+  "use server";
+  const response = await fetch(
+    `${API_URL}complaints/solutions?complaint=${encodeURIComponent(complaint)}`
+  );
+
+  if (response.ok) {
+    const json = await response.json();
+    return json.solution;
+  } else {
+    console.error("Error fetching solution:", response.statusText);
+    return null;
+  }
+};
+
+const handleResolve = async (complaint: any, solution: string | undefined) => {
+    "use server";
+    const index = pc.index("complaints");
+    const prevResolved = complaint.resolved === "true";
+    console.log(prevResolved);
+    console.log(complaint.pinecodeID);
+    try {
+      if (prevResolved) {
+        // Mark as unresolved
+        await index.namespace("rag_complaints").update({
+          id: complaint.pinecodeID,
+          metadata: {
+            resolved: false,
+            admin_text: " ",
+          },
+        });
+      } else {
+        // Mark as resolved with solution
+        await index.namespace("rag_complaints").update({
+          id: complaint.pinecodeID,
+          metadata: {
+            resolved: true,
+            admin_text: solution,
+          },
+        });
+      }
+      await fetchComplaints(complaint.userID);
+    } catch (error) {
+      console.error("Error resolving complaint:", error);
+    }
+  };
+
+export default async function RequestsPage() {
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  if (error) {
+    console.error("Error fetching user:", error);
+    return <div>Error fetching user</div>;
+  }
+
+  if (user) {
+    const userId = user.id || "";
+    const complaints = await fetchComplaints(userId);
+    console.log(complaints);
+    return (
+      <RequestsUI
+        complaints={complaints}
+        fetchSimilarComplaints={fetchSimilarComplaints}
+        fetchSolution={fetchSolution}
+        handleResolve={handleResolve}
+      />
+    );
+  }
+
+  return <div>No user found</div>;
 }
--- src/app/requests/ui.tsx
+++ src/app/requests/ui.tsx
@@ -1,104 +1,226 @@
-import { createClient } from "@/app/utils/supabase/client";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { Pinecone } from "@pinecone-database/pinecone";
+"use client";
+import { useState } from "react";
 
 interface Complaint {
   complaints: any[];
+  fetchSimilarComplaints: (complaint: any) => any;
+  fetchSolution: (complaint: any) => any;
+  handleResolve: (complaint: any, solution: string | undefined) => any;
 }
+
 export default function RequestsUI(props: Complaint) {
   const complaints = props.complaints;
-  // const [complaints, setComplaints] = useState<
-  //   {
-  //     admin_text: string;
-  //     product: string;
-  //     resolved: string;
-  //     subcategory: string;
-  //     summary: string;
-  //     text: string;
-  //     userID: string;
-  //   }[]
-  // >([]);
-  const [userId, setUserId] = useState("");
+  const fetchSimilarComplaints = props.fetchSimilarComplaints;
+  const fetchSolution = props.fetchSolution;
+  const handleResolve = props.handleResolve;
 
-  const router = useRouter();
-  const supabase = createClient();
-  const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! });
+  const [selectedComplaint, setSelectedComplaint] = useState<any>(null);
+  const [similarComplaints, setSimilarComplaints] = useState<any[]>([]);
+  const [solution, setSolution] = useState<any>(null);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [resolvedComplaints, setResolvedComplaints] = useState(
+    complaints.filter((complaint) => complaint.resolved === "true")
+  );
+  const [unresolvedComplaints, setUnresolvedComplaints] = useState(
+    complaints.filter((complaint) => complaint.resolved === "false")
+  );
 
-  // useEffect(() => {
-  //   async function getUserInfo() {
-  //     const {
-  //       data: { user },
-  //       error,
-  //     } = await supabase.auth.getUser();
-  //     if (error) {
-  //       console.error("Error fetching user:", error);
-  //     } else if (user) {
-  //       setUserId(user.id || "");
-  //     }
-  //   }
-
-  //   getUserInfo();
-  // });
-
-  useEffect(() => {
-    async function fetchComplaints() {
-      const index = pc.index("complaints");
-
-      const queryResponse = await index.namespace("rag_complaints").query({
-        vector: Array(1536).fill(0),
-        topK: 10000,
-        includeMetadata: true,
-        filter: {
-          genre: { $eq: userId },
-        },
-      });
-
-      for (const match of queryResponse.matches) {
-        if (match.metadata) {
-          const complaint = {
-            admin_text: match.metadata.admin_text.toString(),
-            product: match.metadata.product.toString(),
-            resolved: match.metadata.resolved.toString(),
-            subcategory: match.metadata.subcategory.toString(),
-            summary: match.metadata.summary.toString(),
-            text: match.metadata.text.toString(),
-            userID: match.metadata.userID.toString(),
-          };
-          setComplaints((complaints) => [...complaints, complaint]);
-        }
-      }
+  const handleComplaintClick = async (complaint: any) => {
+    try {
+      setIsLoading(true);
+      setSelectedComplaint(complaint);
+      const response = await fetchSimilarComplaints(complaint.text);
+      setSimilarComplaints(response);
+      setIsDialogOpen(true);
+    } catch (error) {
+      console.error("Error fetching similar complaints:", error);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    fetchComplaints();
-  }, []);
+  const handleSolutionClick = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetchSolution(selectedComplaint.text);
+      setSolution(response);
+    } catch (error) {
+      console.error("Error fetching solution:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-  const resolvedComplaints = complaints.filter(
-    (complaint) => complaint.resolved === "true"
-  );
-  const unresolvedComplaints = complaints.filter(
-    (complaint) => complaint.resolved === "false"
-  );
+  const closeDialog = () => {
+    setIsDialogOpen(false);
+    setSolution(null);
+  };
+
+  const handleResolveClick = async () => {
+    try {
+      setIsLoading(true);
+      await handleResolve(selectedComplaint, solution);
+      const updatedComplaint = {
+        ...selectedComplaint,
+        resolved: selectedComplaint.resolved === "true" ? "false" : "true",
+      };
+
+      if (updatedComplaint.resolved === "true") {
+        setResolvedComplaints([...resolvedComplaints, updatedComplaint]);
+        setUnresolvedComplaints(
+          unresolvedComplaints.filter(
+            (complaint) => complaint.id !== updatedComplaint.id
+          )
+        );
+      } else {
+        setUnresolvedComplaints([...unresolvedComplaints, updatedComplaint]);
+        setResolvedComplaints(
+          resolvedComplaints.filter(
+            (complaint) => complaint.id !== updatedComplaint.id
+          )
+        );
+      }
+
+      closeDialog();
+    } catch (error) {
+      console.error("Error resolving complaint:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <div>
-      <h1>Requests</h1>
-      <div>
-        <h2>Unresolved Complaints</h2>
-        {unresolvedComplaints.map((complaint, index) => (
-          <div key={index}>
-            <p>{complaint.summary}</p>
-          </div>
-        ))}
+    <div className="container mx-auto">
+      <h1 className="text-center text-2xl font-bold mb-4">Requests</h1>
+      <div className="flex justify-between">
+        <div className="w-1/2 p-4 mr-2">
+          <h2 className="text-xl text-center font-semibold mb-2">
+            Unresolved Complaints
+          </h2>
+          {unresolvedComplaints.map((complaint, index) => (
+            <div
+              key={index}
+              className="mb-2 p-2 border rounded bg-white text-black"
+              onClick={() => handleComplaintClick(complaint)}
+            >
+              <p className="mb-2">
+                <strong>Summary:</strong> {complaint.summary}
+              </p>
+              <p className="mb-2">
+                <strong>Product:</strong> {complaint.product}
+              </p>
+              <p className="mb-2">
+                <strong>Subcategory:</strong> {complaint.subcategory}
+              </p>
+              <p className="mb-4">
+                <strong>Text:</strong> {complaint.text}
+              </p>
+              <p className="text-sm">
+                <strong>User ID:</strong> {complaint.userID}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="w-1/2 p-4 ml-2">
+          <h2 className="text-xl text-center font-semibold mb-2">
+            Resolved Complaints
+          </h2>
+          {resolvedComplaints.map((complaint, index) => (
+            <div
+              key={index}
+              className="mb-2 p-2 border rounded bg-white text-black"
+              onClick={() => handleComplaintClick(complaint)}
+            >
+              <p>
+                <strong>Summary:</strong> {complaint.summary}
+              </p>
+              <p>
+                <strong>Product:</strong> {complaint.product}
+              </p>
+              <p>
+                <strong>Subcategory:</strong> {complaint.subcategory}
+              </p>
+              <p>
+                <strong>Text:</strong> {complaint.text}
+              </p>
+              {complaint.admin_text.trim() !== "" && (
+                <p>
+                  <strong>Admin Text:</strong> {complaint.admin_text}
+                </p>
+              )}
+              <p>
+                <strong>User ID:</strong> {complaint.userID}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
-      <div>
-        <h2>Resolved Complaints</h2>
-        {resolvedComplaints.map((complaint, index) => (
-          <div key={index}>
-            <p>{complaint.summary}</p>
+
+      {isDialogOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 overflow-y-auto py-8">
+          <div className="bg-white p-4 rounded shadow-lg w-1/3 text-black max-h-full overflow-y-auto">
+            <h2 className="text-xl font-semibold mb-2">Similar Complaints</h2>
+            {similarComplaints.map((complaint, index) => (
+              <div key={index} className="mb-2 p-2 border rounded bg-gray-100">
+                <p>
+                  <strong>Product:</strong> {complaint.product}
+                </p>
+                <p>
+                  <strong>Subcategory:</strong> {complaint.subcategory}
+                </p>
+                <p>
+                  <strong>Text:</strong> {complaint.text}
+                </p>
+              </div>
+            ))}
+
+            {solution && (
+              <div className="mt-4 p-2 border rounded bg-green-100">
+                <h3 className="text-lg font-semibold">Solution</h3>
+                <p>{solution}</p>
+              </div>
+            )}
+            <div className="mt-4 flex justify-between">
+              {solution && (
+                <button
+                  className="mr-2 p-2 bg-green-500 text-white rounded"
+                  onClick={handleResolveClick}
+                >
+                  Resolve
+                </button>
+              )}
+              {selectedComplaint.resolved === "true" && (
+                <button
+                  className="mr-2 p-2 bg-yellow-500 text-white rounded"
+                  onClick={handleResolveClick}
+                >
+                  Unresolve
+                </button>
+              )}
+              {selectedComplaint.resolved !== "true" && !solution && (
+                <button
+                  className="p-2 bg-blue-500 text-white rounded"
+                  onClick={handleSolutionClick}
+                >
+                  Generate Solution
+                </button>
+              )}
+              <button
+                className="p-2 bg-red-500 text-white rounded"
+                onClick={closeDialog}
+              >
+                Close
+              </button>
+            </div>
           </div>
-        ))}
-      </div>
+        </div>
+      )}
+      {isLoading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50 text-white">
+          <h1>Loading...</h1>
+        </div>
+      )}
     </div>
   );
 }
